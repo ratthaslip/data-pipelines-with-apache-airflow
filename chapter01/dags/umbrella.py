1dd3ca381e4a2818bec72680997a76db86e25896
ratthaslip <89380151+ratthaslip@users.noreply.github.com>
2021-09-20 21:32:47 +0700

    Update umbrella.py

--- chapter01/dags/umbrella.py
+++ chapter01/dags/umbrella.py
@@ -1,22 +1,29 @@
-"""DAG demonstrating the umbrella use case with dummy operators."""
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 
-import airflow.utils.dates
-from airflow import DAG
-from airflow.operators.dummy import DummyOperator
+#g are defaults which can be overridden later on
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 4, 15),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
 
-dag = DAG(
-    dag_id="umbrella",
-    description="Umbrella example with DummyOperators.",
-    start_date=airflow.utils.dates.days_ago(5),
-    schedule_interval="@daily",
-)
+dag = DAG('umbrella', default_args=default_args)
 
-fetch_weather_forecast = DummyOperator(task_id="fetch_weather_forecast", dag=dag)
-fetch_sales_data = DummyOperator(task_id="fetch_sales_data", dag=dag)
-clean_forecast_data = DummyOperator(task_id="clean_forecast_data", dag=dag)
+
+fetch_weather_forecast = BashOperator(task_id="fetch_weather_forecast",bash_command='echo "fetch_weatger_forecast"', dag=dag)
+fetch_sales_data = BashOperator(task_id="fetch_sales_data",bash_command='echo "fetch_sales_data"', dag=dag)
 
 
 
 # Set dependencies between all tasks
-fetch_sales_data >> clean_sales_data
+fetch_weather_forecast  >> clean_forecast_data
+
 
