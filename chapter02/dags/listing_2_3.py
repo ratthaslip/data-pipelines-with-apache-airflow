be37e8d3c36d87c79f34328af6e9bae669016d75
ratthaslip <89380151+ratthaslip@users.noreply.github.com>
2021-09-20 22:01:31 +0700

    Update listing_2_3.py

--- chapter02/dags/listing_2_3.py
+++ chapter02/dags/listing_2_3.py
@@ -1,8 +1,26 @@
-import airflow
+import json
+import pathlib
+
+
+import requests
+import requests.exceptions as requests_exceptions
+
+from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 4, 15),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
 
 dag = DAG(
-    dag_id="listing_2_03",
-    start_date=airflow.utils.dates.days_ago(14),
-    schedule_interval=None,
-)
+    dag_id="listing_2_3",
+    description="Download rocket pictures of recently launched rockets.",
+    default_args=default_args)
+}
